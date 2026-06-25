f443697076ce9598ab30e636c716e3391a76b622
CedesAJ <76976287+CedesAJ@users.noreply.github.com>
2026-06-25 02:01:10 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
     const vacationDate = new Date("May 29, 2026 00:00:00").getTime();
-
+    const vacationEnd = new Date("June 3, 2026 00:00:00").getTime();
+        
     function updateCountdown() {
       const now = new Date().getTime();
       const distance = vacationDate - now;
       const days = Math.ceil(distance / (1000 * 60 * 60 * 24));
       if(distance > 0){
         document.getElementById("days").innerHTML = days;
+      } else if {
+        document.getElementById("days").style.display = "none";
+        document.querySelector(".label").style.display = "none";
+        document.getElementById("message").innerHTML =
+          "Your Sint Maarten vacation has ended😕";
       } else {
         document.getElementById("days").style.display = "none";
         document.querySelector(".label").style.display = "none";
